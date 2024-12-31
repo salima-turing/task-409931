b5e3b8966be84a1dddacf75e0baaba3a615b892a
salima-turing <salima.o@turing.com>
2024-12-31 07:05:03 +0100

    Create notification.js

---
+++ notification.js
@@ -0,0 +1,10 @@
+// notification.js
+
+class Notification {
+    static sendAlert(message) {
+        console.log("Sending alert:", message);
+        // Implement actual notification logic (e.g., email, SMS, webhook)
+    }
+}
+
+module.exports = Notification;
